b69d8ceb81c9f7bfd1b781c315c4107e521a3477
Marcelo Raineri <mraineridev@gmail.com>
2025-06-04 23:10:35 -0300

    feat(migrations): adding necessary fields that had not yet been implemented

--- database/migrations/2025_06_04_221817_create_vouchers.php
+++ database/migrations/2025_06_04_221817_create_vouchers.php
@@ -13,12 +13,17 @@ return new class extends Migration
     {
         Schema::create('vouchers', function (Blueprint $table) {
             $table->id();
+            $table->boolean('active')->default(true);
             $table->string('name', 255);
             $table->string('code', 255);
-            $table->timestamp('added_at', precision: 0);
+            $table->integer('quantity');
+            $table->timestamp('start_at', precision: 0);
+            $table->timestamp('end_at', precision: 0);
+            $table->decimal('min_value', total: 10, places : 2);
             $table->timestamps();
             $table->softDeletes('deleted_at', precision: 0);
         });
+
     }
 
     /**
