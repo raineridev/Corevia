d3ac5fd7052bda66526e2de749f973e1b852cede
Marcelo Raineri <mraineridev@gmail.com>
2025-06-06 16:25:50 -0300

    feat(services): implement ProductService to handle business logic abstraction

---
+++ app/Http/Services/ProductService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\DTOs\BaseDTO;
+use App\DTOs\Products\ProductData;
+use App\Http\Repositories\Eloquent\ProductRepository;
+
+class ProductService
+{
+    public function __construct(private ProductRepository $productRepository) {}
+    public function all()
+    {
+        return $this->productRepository->all();
+    }
+
+    public function create(ProductData $data)
+    {
+        return $this->productRepository->create($data);
+    }
+
+    public function find(int $id)
+    {
+        return $this->productRepository->find($id);
+    }
+    public function update(int $id, array $data)
+    {
+        return $this->productRepository->update($id, $data);
+    }
+
+    public function delete($id)
+    {
+        return $this->productRepository->delete($id);
+    }
+}
