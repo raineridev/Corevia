b291feeccaea11c365d12f243cd1100c4519efd8
Marcelo Raineri <mraineridev@gmail.com>
2025-06-06 16:28:21 -0300

    feat(dtos): add ProductData DTO to encapsulate required fields for product creation

---
+++ app/DTOs/Products/ProductData.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\DTOs\Products;
+
+use App\DTOs\BaseDTO;
+
+class ProductData extends BaseDTO{
+        public function __construct(
+        public bool $active,
+        public string $name,
+        public float $price,
+        public int $stock
+    ) {}
+}
