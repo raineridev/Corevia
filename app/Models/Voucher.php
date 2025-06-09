2160ffe4ff047e817404f803926924bce083096b
Marcelo Raineri <mraineridev@gmail.com>
2025-06-09 11:48:04 -0300

    refactor(dtos): implementation for attributes to be assigned in mass

--- app/Models/Voucher.php
+++ app/Models/Voucher.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Voucher extends Model
 {
-    //
+    protected $fillable = [
+        'active',
+        'name',
+        'code',
+        'quantity',
+        'start_at',
+        'end_at',
+        'min_value'
+    ];
 }
