2e5da345ecfe03a7d834e11d3a40d2cd914c2989
Marcelo Raineri <mraineridev@gmail.com>
2025-06-10 11:14:27 -0300

    chore: add request validation rules checking product name, price and id

---
+++ app/Http/Requests/Stock/StoreStockRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreStockRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' =>  'required|string|max:255',
+            'price' =>  "required |numeric|regex:/^\d+(\.\d{1,2})?$/|gt:0",
+            'product_id' =>  'required|exists:products,id',
+        ];
+    }
+}
