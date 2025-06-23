3bbc750405853f9bfd19b63c693d7da529f62864
Marcelo Raineri <mraineridev@gmail.com>
2025-06-23 00:41:00 -0300

    chore(routes): add resource routes for products, voucher, variant, and stock; import controllers

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StockController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VoucherController;
+use App\Http\Controllers\VariantController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::resource('products', ProductController::class);
+Route::resource('voucher', VoucherController::class);
+Route::resource('variant', VariantController::class);
+Route::resource('stock', StockController::class);
