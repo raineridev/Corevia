142f267132f0ab4fbb44fd62afb725238964739e
Marcelo Raineri <mraineridev@gmail.com>
2025-06-23 00:39:05 -0300

    fix(dto): refactor StockData DTO, remove unused name property, make variantID optional

--- app/DTOs/StockData.php
+++ app/DTOs/StockData.php
@@ -5,9 +5,8 @@ namespace App\DTOs;
 class StockData extends BaseDTO
 {
     public function __construct(
-        public string $name,
         public float $quantity,
         public int $productID,
-        public string $variantID
+        public ?string $variantID = null,
     ) {}
 }
